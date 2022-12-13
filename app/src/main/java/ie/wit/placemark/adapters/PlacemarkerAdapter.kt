999867147f518d269455311c143d0090f7f960cd
Decodon <decodon8@gmail.com>
2022-12-13 22:39:36 +0000

    Exercise 3/4 - Image View refreshIntentLaunche

--- app/src/main/java/ie/wit/placemark/adapters/PlacemarkerAdapter.kt
+++ app/src/main/java/ie/wit/placemark/adapters/PlacemarkerAdapter.kt
@@ -3,6 +3,7 @@ package ie.wit.placemark.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import ie.wit.placemark.databinding.CardPlacemarkBinding
 import ie.wit.placemark.models.PlacemarkModel
 
@@ -33,6 +34,7 @@ class PlacemarkAdapter constructor(private var placemarks: List<PlacemarkModel>,
         fun bind(placemark: PlacemarkModel, listener: PlacemarkListener) {
             binding.placemarkTitle.text = placemark.title
             binding.description.text = placemark.description
+            Picasso.get().load(placemark.image).resize(200,200).into(binding.imageIcon)
             binding.root.setOnClickListener { listener.onPlacemarkClick(placemark) }
         }
     }
