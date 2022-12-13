e5b71e8a4d8422654ee4ce6cdfdb56edd54cc81b
Decodon <decodon8@gmail.com>
2022-12-13 22:24:18 +0000

    Displaying the (Placemark) Image Displaying the image in Edit mode

--- app/src/main/java/ie/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/ie/wit/placemark/activities/PlacemarkActivity.kt
@@ -8,6 +8,7 @@ import android.view.MenuItem
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.snackbar.Snackbar
+import com.squareup.picasso.Picasso
 import ie.wit.placemark.R
 import ie.wit.placemark.databinding.ActivityPlacemarkBinding
 import ie.wit.placemark.helpers.showImagePicker
@@ -38,6 +39,9 @@ class PlacemarkActivity : AppCompatActivity() {
             binding.placemarkTitle.setText(placemark.title)
             binding.description.setText(placemark.description)
             binding.btnAdd.setText(R.string.save_placemark)
+            Picasso.get()
+                .load(placemark.image)
+                .into(binding.placemarkImage)
         }
 
         binding.btnAdd.setOnClickListener() {
@@ -88,6 +92,10 @@ class PlacemarkActivity : AppCompatActivity() {
                     RESULT_OK -> {
                         if (result.data != null) {
                             i("Got Result ${result.data!!.data}")
+                            placemark.image = result.data!!.data!!
+                            Picasso.get()
+                                .load(placemark.image)
+                                .into(binding.placemarkImage)
                         } // end of if
                     }
                     RESULT_CANCELED -> { } else -> { }
