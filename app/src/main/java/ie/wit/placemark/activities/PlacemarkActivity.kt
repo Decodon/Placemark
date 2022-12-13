50a34ba2819a8bb3c6e8044ac77105dfa1b6fa63
Decodon <decodon8@gmail.com>
2022-12-13 22:30:14 +0000

    Exercise 1/2 - Images changes when update. Change Button to be Change Image in Edit mode

--- app/src/main/java/ie/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/ie/wit/placemark/activities/PlacemarkActivity.kt
@@ -1,6 +1,7 @@
 package ie.wit.placemark.activities
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -42,6 +43,9 @@ class PlacemarkActivity : AppCompatActivity() {
             Picasso.get()
                 .load(placemark.image)
                 .into(binding.placemarkImage)
+            if (placemark.image != Uri.EMPTY) {
+                binding.chooseImage.setText(R.string.change_placemark_image)
+            }
         }
 
         binding.btnAdd.setOnClickListener() {
@@ -96,6 +100,7 @@ class PlacemarkActivity : AppCompatActivity() {
                             Picasso.get()
                                 .load(placemark.image)
                                 .into(binding.placemarkImage)
+                            binding.chooseImage.setText(R.string.change_placemark_image)
                         } // end of if
                     }
                     RESULT_CANCELED -> { } else -> { }
