092d74661d2a0c76db672f0f4072215fa8451b77
Decodon <decodon8@gmail.com>
2022-12-18 11:02:28 +0000

    MainApp & PlacemarkListActivity Adjustments & Additional Helper Files

--- app/src/main/java/ie/wit/placemark/main/MainApp.kt
+++ app/src/main/java/ie/wit/placemark/main/MainApp.kt
@@ -3,17 +3,20 @@ package ie.wit.placemark.main
 import android.app.Application
 import ie.wit.placemark.models.PlacemarkMemStore
 import ie.wit.placemark.models.PlacemarkModel
+import ie.wit.placemark.models.PlacemarkStore
 import timber.log.Timber
 import timber.log.Timber.i
 
 class MainApp : Application() {
 
     //val placemarks = ArrayList<PlacemarkModel>()
-    val placemarks = PlacemarkMemStore()
+    //val placemarks = PlacemarkMemStore()
+    lateinit var placemarks: PlacemarkStore
 
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        placemarks = PlacemarkMemStore()
         i("Placemark started")
     }
 }
